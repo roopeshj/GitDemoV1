58cf1637aeeded66bc21ea0e55a6ca263d4ceb99
roopeshj <jroopesh14@gmail.com>
2021-06-01 21:14:04 +0530

    This is my first commit..

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if (hungry == "yes")
+			System.out.println("Hungry");
+		else {
+			System.out.println("No Hungry");
+		}
+	}
+
+}
