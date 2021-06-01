dddacacbcbd14c1851884e9a4947846460887e73
SJR-Demo <roopesh.vil@gmail.com>
2021-06-01 23:42:44 +0530

    Second Commit

--- src/Test2.java
+++ src/Test2.java
@@ -2,7 +2,7 @@
 public class Test2 {
 	public static void main(String[] args) {
 		String hungry = "yes";
-		if (hungry == "yes")
+		if (hungry == "yes" || hungry == "Y" || hungry == "y")
 			System.out.println("eating");
 		else {
 			System.out.println("Work");
